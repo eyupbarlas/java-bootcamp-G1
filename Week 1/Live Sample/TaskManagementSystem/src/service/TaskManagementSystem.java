e764bcf3320d174fbeda0fb531ec3b07a1dfebbf
Bzzman <eyupbarlas2134@gmail.com>
2024-03-06 17:57:42 +0100

    completing task1

--- Week 1/Live Sample/TaskManagementSystem/src/service/TaskManagementSystem.java
+++ Week 1/Live Sample/TaskManagementSystem/src/service/TaskManagementSystem.java
@@ -60,5 +60,7 @@ public class TaskManagementSystem {
     public static void checkOverdueTasks() {
         // implement checkOverdueTasks logic
         System.out.println("task is complete");
+        System.out.println("task is complete");
+
     }
 }
