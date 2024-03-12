546de7e58f48d3ddec1700c4481f740e15383fa5
Tiger <98557920+hasanmertyilmaz@users.noreply.github.com>
2024-03-12 23:42:56 +0100

    Update Week1Task1.java

--- Homeworks/src/Week1/Week1Task1.java
+++ Homeworks/src/Week1/Week1Task1.java
@@ -1,4 +1,19 @@
+// Task 3
+
 package Week1;
 
 public class Week1Task1 {
+          Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the radius of the circle: ");
+        double radius = scanner.nextDouble();
+
+        
+        double area = Math.PI * radius * radius;
+
+        
+        double perimeter = 2 * Math.PI * radius;
+
+        System.out.println("Area of the circle: " + area);
+        System.out.println("Perimeter (Circumference) of the circle: " + perimeter);
 }
